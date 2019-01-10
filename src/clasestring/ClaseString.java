30f588db40ec98eff5884be99776cc5cce71234c
Your Name <y@m.es>
2019-01-10 13:17:40 +0100

    Métodos length() y charAt()

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -13,6 +13,14 @@ public class ClaseString {
 
    
     public static void main(String[] args) {
+        
+        String s = "iesmardealboran.com";
+        
+        // Tamaño de la cadena
+        System.out.println("Tamaño: " + s.length());
+        
+        // Obtener el char de una posicion concreta
+        System.out.println("char de la posicion 5: " + s.charAt(5));
        
     }
     
